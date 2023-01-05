e15bd80884d1e588a9ff60c60ae9b90f66f917dc
Jorge Meza Bello <jorberiver@gmail.com>
2023-01-04 20:14:00 -0600

    Cambios hechos en mapa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -2,15 +2,31 @@
 
     const lat = 19.0470994;
     const lng = -98.1726242;
-    const mapa = L.map('mapa').setView([lat, lng ], 16);
+    const mapa = L.map('mapa').setView([lat, lng ], 17);
     let marker;
+    const geocodeService = L.esri.Geocoding.geocodeService();
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+    //PIN
     marker = new L.marker([lat, lng], {
         draggable: true,
         autoPan: true
     })
     .addTo(mapa)
+    //Detectar el movimiento del pin
+    marker.on("moveend", function(e){
+        marker = e.target;
+        const position = marker.getLatLng();
+        mapa.panTo(new L.LatLng(position.lat, position.lng))
+        //Obtener informacion de las calles
+        geocodeService.reverse().latlng(position, 16).run(function (error, resultado){
+            marker.bindPopup(resultado.address.LongLabel);
+            //Llenar los campos
+            document.querySelector(".calle").textContent = resultado.address.Address ?? ""
+        })
+    })
+
+
 })()
