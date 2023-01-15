78533b55e584342ca29c1209c4daf61b8b3ce5ee
Jorge Meza Bello <jorberiver@gmail.com>
2023-01-15 00:20:38 -0600

    Propiedades en JSON

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,8 +1,13 @@
+import { Propiedad, Precio, Categoria } from "../models/index.js"
 
-const propiedades = (req, res) => {
-    res.json({
-        respuesta: "Ok"
+const propiedades = async (req, res) => {
+    const propiedades = await Propiedad.findAll({
+        include: [
+            {model: Precio, as: "precio"},
+            {model: Categoria, as: "categoria"}
+        ]
     })
+    res.json(propiedades)
 }
 
 export { propiedades }
