196117ee0771dc1f20e69384705d082f5b26dc09
Jorge Meza Bello <jorberiver@gmail.com>
2023-01-16 01:59:16 -0600

    Vista principal lista

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,15 +1,43 @@
 import { Precio, Categoria, Propiedad } from "../models/index.js";
 
 const inicio = async (req, res) => {
-    const [categorias, precios] = await Promise.all([
+    const [categorias, precios, apartamentos, casas] = await Promise.all([
         Categoria.findAll({ raw: true }),
-        Precio.findAll()
+        Precio.findAll(),
+        Propiedad.findAll({
+            limit: 3,
+            where: {
+                categoriaId: 1
+            },
+            include: [
+                {
+                    model: Precio,
+                    as: "precio"
+                }
+            ],
+            order: [["createdAt", "DESC"]]
+        }),
+        Propiedad.findAll({
+            limit: 3,
+            where: {
+                categoriaId: 2
+            },
+            include: [
+                {
+                    model: Precio,
+                    as: "precio"
+                }
+            ],
+            order: [["createdAt", "DESC"]]
+        })
     ])
 
     return res.render("inicio", {
         pagina: "Inicio",
         categorias,
-        precios
+        precios,
+        casas,
+        apartamentos
     });
 }
 
