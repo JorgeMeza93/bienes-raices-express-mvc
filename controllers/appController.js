341d6570916b95e0a6ea40c7489b71a0c1f545a4
Jorge Meza Bello <jorberiver@gmail.com>
2023-01-16 02:27:34 -0600

    Paginas de categorias listas

--- controllers/appController.js
+++ controllers/appController.js
@@ -41,8 +41,25 @@ const inicio = async (req, res) => {
     });
 }
 
-const categoria = (req, res) => {
-
+const categoria = async (req, res) => {
+    const { id } = req.params;
+    const categoria = await Categoria.findByPk(id)
+    if( !categoria ){
+        return res.render("/404")
+    }
+    // Obtener las propiedades de categoria
+    const propiedades = await Propiedad.findAll({
+        where: {
+            categoriaId: id
+        },
+        include: [
+            {model: Precio, as: "precio"}
+        ]
+    })
+    res.render("categoria", {
+        pagina: `${categoria.nombre}s en Venta`,
+        propiedades
+    })
 }
 
 const notFound404 = (req, res) => {
