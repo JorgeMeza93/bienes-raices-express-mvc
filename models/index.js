a5a71ee3e5f08f2db5380e502a54f7fa54ecbadf
Jorge Meza Bello <jorberiver@gmail.com>
2023-01-17 23:19:52 -0600

    Creacion del modelo Mensaje

--- models/index.js
+++ models/index.js
@@ -2,14 +2,19 @@ import Propiedad from "./Propiedad.js";
 import Precio from "./Precio.js";
 import Categoria from "./Categoria.js";
 import Usuario from "./Usuario.js";
+import Mensaje from "./Mensaje.js";
 
 Propiedad.belongsTo(Precio, { foreignKey: "precioId"});
 Propiedad.belongsTo(Categoria, { foreignKey: "categoriaId"});
 Propiedad.belongsTo(Usuario, { foreignKey: "usuarioId"})
 
+Mensaje.belongsTo(Propiedad, { foreignKey: "propiedadId" });
+Mensaje.belongsTo(Usuario, { foreignKey: "usuarioId" });
+
 export {
     Propiedad,
     Precio,
     Categoria,
-    Usuario
+    Usuario,
+    Mensaje
 }
